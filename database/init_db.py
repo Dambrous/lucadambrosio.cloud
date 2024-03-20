4c1e3525ad7e4735ff54fcd1f74ebf7a38fe44ee
Dambrous <91986538+Dambrous@users.noreply.github.com>
2024-03-20 19:03:04 +0100

    [DEPLOY] full portfolio website with REACT/TYPESCRIPT

--- database/init_db.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-connection = sqlite3.connect('lucadambrosio.db')
-
-with open('db.sql') as script_sql:
-    connection.executescript(script_sql.read())
-
-connection.close()
--- site_without_server_side/static/js/script.js
+++
@@ -1,8 +0,0 @@
-var currentUrl = window.location.pathname;
-
-var navbarLinks = document.querySelectorAll('.custom-link');
-navbarLinks.forEach(function (link) {
-    if (link.getAttribute('href') === currentUrl) {
-        link.classList.add('active');
-    }
-});
